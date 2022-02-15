ba4f2fa1dc0db2ef44801d01ce6abc538ed55528
Eusebiu Popescu <popescusebi1234@yahoo.com>
2022-02-15 14:42:48 +0200

    Update restanta.scala

--- src/main/scala/restanta.scala
+++ src/main/scala/restanta.scala
@@ -10,17 +10,15 @@ object Ex1 {
 
     abstract class State {
         val pret:Element[String]
-        val vanzari:Element[String]
-    }
-
-    class Initial extends State {
-        val pret = Select(0.3 -> "scade", 0.3 -> "stagneaza", 0.3 -> "creste")
-        val vanzari = 
-        CPD(pret,
+        def vanzari() = CPD(pret,
         "scade" -> Select(0.5 -> "ridicat", 0.2 -> "mediu", 0.15 -> "scazut", 0.05 -> "foarte scazut"),
         "stagneaza" -> Select(0.3 -> "ridicat", 0.3 -> "mediu", 0.2 -> "scazut", 0.1 -> "foarte scazut"),
         "creste" -> Select(0.2 -> "ridicat", 0.2 -> "mediu", 0.2 -> "scazut", 0.4 -> "foarte scazut")
         )
+    }
+
+    class InitialState extends State {
+        val pret = Select(0.3 -> "scade", 0.3 -> "stagneaza", 0.3 -> "creste")   
     }
 
     class NextState(current: State) extends State {
@@ -34,15 +32,38 @@ object Ex1 {
 
 	def main(args: Array[String]) {
         val an = 12
+        val vanzariLuni:Array[State] = Array.fill(an)(new InitialState());
+
+        for { i <- 1 until an } {
+            vanzariLuni(i) = new NextState(vanzariLuni(i-1));
+        }
+
+        vanzariLuni(0).vanzari.observe("ridicat")
+        vanzariLuni(1).vanzari.observe("ridicat")
+        vanzariLuni(2).vanzari.observe("mediu")
+
+        println("Sansa ca in luna 4 sa scada " + VariableElimination.probability(vanzariLuni(3).pret, "scade"))
+        println("Sansa ca in luna 4 sa stagneze " + VariableElimination.probability(vanzariLuni(3).pret, "stagneaza"))
+        println("Sansa ca in luna 4 sa creasca " + VariableElimination.probability(vanzariLuni(3).pret, "creste"))
+
+        /*
+        Sansa ca in luna 4 sa scada 0.2511419083524646
+        Sansa ca in luna 4 sa stagneze 0.48189445158210303
+        Sansa ca in luna 4 sa creasca 0.26696364006543244
+        */
+
+        vanzariLuni(0).vanzari.unobserve()
+        vanzariLuni(1).vanzari.unobserve()
+        vanzariLuni(2).vanzari.unobserve()
+
+        vanzariLuni(4).vanzari.observe("scazut")
+        vanzariLuni(5).vanzari.observe("scazut")
+        println("Sansa ca in luna 2 preturile sa scada " + Importance.probability(vanzariLuni(1).pret, "scade"))
+        println("Sansa ca in luna 3 preturile sa scada " + Importance.probability(vanzariLuni(2).pret, "scade"))
+
+        /*
+        Sansa ca in luna 2 preturile sa scada 0.2909290625680987
+        Sansa ca in luna 3 preturile sa scada 0.1920933474277721
+        */
 	}
-class Nominalizare(val album: Album) {
-   def getProb(calitate: Symbol, popularitate: Boolean) =
-     (calitate, popularitate) match {
-       case ('mica, false) => 0.003
-       case ('mica, true) => 0.014
-       case ('medie, false) => 0.016
-       case ('medie, true) => 0.043
-       case ('mare, false) => 0.047
-       case ('mare, true) => 0.18
-     }
 }
